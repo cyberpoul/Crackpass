9b76b051c0a74e547dbf48ebcc168648f51a557e
Assa <143470757+Assa228@users.noreply.github.com>
2024-07-28 04:25:33 +0200

    Update crackpass.sh

--- crackpass.sh
+++ crackpass.sh
@@ -9,7 +9,7 @@ echo "*                                               *"
 echo "*************************************************"
 
 if [ $# -ne 1 ]; then
-  echo "Usage : $0 <fichier.rar, fichier.zip or fichier.pdf>"
+  echo "Usage : $0 <fichier.rar, fichier.zip , fichier.docx or fichier.pdf>"
   exit 1
 fi
 
@@ -23,7 +23,11 @@ elif [ "$extension" == "zip" ]; then
   zip2john "$fichier" > hash
   john --wordlist=/usr/share/wordlists/rockyou.txt hash
 elif [ "$extension" == "pdf" ]; then
-  pdfcrack --wordlist=/usr/share/wordlists/rockyou.txt "$fichier"
+  pdf2john "$fichier" > hash
+  john --wordlist=/usr/share/wordlists/rockyou.txt hash
+elif [ "$extension" == "docx" ]; then
+  office2john "$fichier" > hash
+  john --wordlist=/usr/share/wordlists/rockyou.txt hash
 else
   echo "Extension de fichier non supportée : $extension"
   exit 1
